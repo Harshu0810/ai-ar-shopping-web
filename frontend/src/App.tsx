8b7576c6e454a8a62c26250b12cdd28f39a69afb
Harshu0810 <143718146+Harshu0810@users.noreply.github.com>
2026-01-03 20:46:03 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,32 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import Home from './pages/Home'
+import VirtualTryOn from './pages/VirtualTryOn'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/virtual-try-on" element={<VirtualTryOn />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
